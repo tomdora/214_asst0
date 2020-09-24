c43eacd52f009fcab23f5ef9d73461f8c763421c
Thomas Fiorilla <engineertf@gmail.com>
2020-09-24 16:31:46 -0400

    String detection
    Made isString() function, able to detect valid strings within the input.
    Also refactored isKeyword to use less comparisons.

--- tokenizer.c
+++ tokenizer.c
@@ -4,39 +4,28 @@ Node * head = NULL;
 int startLoc = 0;			//starting location
 int currentLoc = 0;			//current iteration location
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//Function to create a head node. Checks to see if the entire string is one single token first.
-void createHeadNode(char * input, char * inputType){
-	//printf("No head.\n");
-	
-	head = (Node*)malloc(sizeof(Node));
-	head->data = malloc(sizeof(char) * currentLoc-startLoc+2);
-	
-	strncpy(head->data, input + startLoc, currentLoc-startLoc+1);
-	head->data[currentLoc+1] = '\0';
-	head->type = inputType;
-	
-	currentLoc++;
-	startLoc = currentLoc;
-	
-	return;
-}
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////// ""Beep boop" beep"
 
 
 //Function to create a new node. Checks to see if the head exists first.
 void createNewNode(char * input, char * inputType){
-	//printf("startLoc: %d, currentLoc: %d\n", startLoc, currentLoc);
+	//printf("type: %s, startLoc: %d, currentLoc: %d\n", inputType, startLoc, currentLoc);
 	
 	//If no head exists, function passes to the function to create a head.
 	if(head == NULL){
 		//printf("Head is null.\n");
-		createHeadNode(input, inputType);
-	} 
+		
+		head = malloc(sizeof(Node));
+		head->data = malloc(sizeof(char) * (currentLoc - startLoc + 2));
+		
+		strncpy(head->data, input + startLoc, currentLoc-startLoc+1);
+		head->data[currentLoc - startLoc+1] = '\0';
+		head->type = inputType;
+		
+		currentLoc++;
+		startLoc = currentLoc;
+	}
 	
 	//Else if a head exists, the code will run as normal to create a new node.
 	else{
@@ -51,7 +40,7 @@ void createNewNode(char * input, char * inputType){
 		l->next = new;
 		
 		strncpy(new->data, input + startLoc, currentLoc-startLoc+1);
-		head->data[currentLoc+1] = '\0';
+		new->data[currentLoc - startLoc+1] = '\0';
 		new->type = inputType;
 		
 		currentLoc++;
@@ -67,76 +56,144 @@ void createNewNode(char * input, char * inputType){
 
 //Function for testing specific words
 void isKeyword(char * input){
-	if(strncmp(input + startLoc, "sizeof", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "sizeof");
-	}
-	else if(strncmp(input + startLoc, "auto", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "auto");
-	} else if(strncmp(input + startLoc, "break", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "break");
-	} else if(strncmp(input + startLoc, "case", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "case");
-	} else if(strncmp(input + startLoc, "char", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "char");
-	} else if(strncmp(input + startLoc, "const", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "const");
-	} else if(strncmp(input + startLoc, "continue", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "continue");
-	} else if(strncmp(input + startLoc, "default", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "default");
-	} else if(strncmp(input + startLoc, "do", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "do");
-	} else if(strncmp(input + startLoc, "double", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "double");
-	} else if(strncmp(input + startLoc, "else", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "else");
-	} else if(strncmp(input + startLoc, "enum", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "enum");
-	} else if(strncmp(input + startLoc, "extern", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "extern");
-	} else if(strncmp(input + startLoc, "float", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "float");
-	} else if(strncmp(input + startLoc, "for", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "for");
-	} else if(strncmp(input + startLoc, "goto", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "goto");
-	} else if(strncmp(input + startLoc, "if", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "if");
-	} else if(strncmp(input + startLoc, "int", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "int");
-	} else if(strncmp(input + startLoc, "long", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "long");
-	} else if(strncmp(input + startLoc, "register", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "register");
-	} else if(strncmp(input + startLoc, "return", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "return");
-	} else if(strncmp(input + startLoc, "short", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "short");
-	} else if(strncmp(input + startLoc, "signed", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "signed");
-	} else if(strncmp(input + startLoc, "sizeof", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "sizeof");
-	} else if(strncmp(input + startLoc, "static", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "static");
-	} else if(strncmp(input + startLoc, "struct", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "struct");
-	} else if(strncmp(input + startLoc, "switch", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "switch");
-	} else if(strncmp(input + startLoc, "typedef", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "typedef");
-	} else if(strncmp(input + startLoc, "union", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "union");
-	} else if(strncmp(input + startLoc, "unsigned", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "unsigned");
-	} else if(strncmp(input + startLoc, "void", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "void");
-	} else if(strncmp(input + startLoc, "volatile", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "volatile");
-	} else if(strncmp(input + startLoc, "while", currentLoc-startLoc+1) == 0){
-		createNewNode(input, "while");
-	}
+	//printf("startLoc: %d, currentLoc: %d\n", startLoc, currentLoc);
 	
-	//If it is none of these tokens, it is a word of its own
+	//Test to see if the token can possibly be any of these operators.
+	if(currentLoc-startLoc+1 > 1 && currentLoc-startLoc+1 < 9){
+		//printf("Between 1 and 9\n");
+		
+		//Split the sizes in half for 2,3,4 or 5,6,7,8
+		//This reduces the number of comparisons necessary from 7 (==2,==3,==4,==5,==6,==7,==8) to either 4 (if,==2,==3,==4) or 5 (if,==5,==6,==7,==8)
+		if(currentLoc-startLoc+1 < 5){
+			//Check for 2
+			if(currentLoc-startLoc+1 == 2){
+				if(strncmp(input + startLoc, "do", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "do");
+				} else if(strncmp(input + startLoc, "if", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "if");
+				} 
+				
+				else{
+					createNewNode(input, "word");
+				}
+			}
+			//Check for 3
+			else if(currentLoc-startLoc+1 == 3){
+				if(strncmp(input + startLoc, "for", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "for");
+				} else if(strncmp(input + startLoc, "int", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "int");
+				} 
+				
+				else{
+					createNewNode(input, "word");
+				}
+			} 
+			//If not 2 or 3 it must be 4
+			else{
+				if(strncmp(input + startLoc, "auto", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "auto");
+				} else if(strncmp(input + startLoc, "case", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "case");
+				} else if(strncmp(input + startLoc, "char", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "char");
+				} else if(strncmp(input + startLoc, "else", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "else");
+				} else if(strncmp(input + startLoc, "enum", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "enum");
+				} else if(strncmp(input + startLoc, "goto", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "goto");
+				} else if(strncmp(input + startLoc, "long", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "long");
+				} else if(strncmp(input + startLoc, "void", currentLoc-startLoc+1) == 0){
+					createNewNode(input, "void");
+				} 
+				
+				else{
+					createNewNode(input, "word");
+				}
+			}
+		} else{
+			//Split 4(==5,==6,==7,==8) into 2/3 (if,==5/,==6) or 2/3 (if,==7/,==8)
+			if(currentLoc-startLoc+1 < 7){
+				//Check if it's 5
+				if(currentLoc-startLoc+1 == 5){
+					if(strncmp(input + startLoc, "break", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "break");
+					} else if(strncmp(input + startLoc, "const", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "const");
+					} else if(strncmp(input + startLoc, "float", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "float");
+					} else if(strncmp(input + startLoc, "short", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "short");
+					} else if(strncmp(input + startLoc, "union", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "union");
+					} else if(strncmp(input + startLoc, "while", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "while");
+					}
+					
+					else{
+						createNewNode(input, "word");
+					}
+				}
+				//If it's not 5, it's 6.
+				else{
+					if(strncmp(input + startLoc, "double", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "double");
+					} else if(strncmp(input + startLoc, "extern", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "extern");
+					} else if(strncmp(input + startLoc, "return", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "return");
+					} else if(strncmp(input + startLoc, "signed", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "signed");
+					} else if(strncmp(input + startLoc, "sizeof", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "sizeof");
+					} else if(strncmp(input + startLoc, "static", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "static");
+					} else if(strncmp(input + startLoc, "struct", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "struct");
+					} else if(strncmp(input + startLoc, "switch", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "switch");
+					} 
+					
+					else{
+						createNewNode(input, "word");
+					}
+				}
+			} 
+			else{
+				//Check if it's 7
+				if(currentLoc-startLoc+1 == 7){
+					if(strncmp(input + startLoc, "default", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "default");
+					} else if(strncmp(input + startLoc, "typedef", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "typedef");
+					} 
+					
+					else{
+						createNewNode(input, "word");
+					}
+				}
+				//If it's not 7, it's 8
+				else{
+					if(strncmp(input + startLoc, "continue", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "continue");
+					} else if(strncmp(input + startLoc, "register", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "register");
+					} else if(strncmp(input + startLoc, "unsigned", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "unsigned");
+					} else if(strncmp(input + startLoc, "volatile", currentLoc-startLoc+1) == 0){
+						createNewNode(input, "volatile");
+					} 
+					
+					else{
+						createNewNode(input, "word");
+					}
+				}
+			}
+		}
+	}
+	//If it's too small or too big to be any of them, it's a word.
 	else{
 		createNewNode(input, "word");
 	}
@@ -154,7 +211,7 @@ void isWord(char * input){
 		
 		//Check to see if the character is alphanumeric. 
 		if(currentLoc < strlen(input)-1 && !isalpha(input[currentLoc+1]) && !isdigit(input[currentLoc+1])){
-			//createNewNode(input, "word");
+			//printf("End of word.\n");
 			
 			isKeyword(input);
 			
@@ -164,8 +221,6 @@ void isWord(char * input){
 		//Check to see if we're at the end of the string, since the function won't automatically send the rest of the string.
 		else if(currentLoc == strlen(input) - 1){
 			//printf("End of string.\n");
-			
-			//createNewNode(input, "word");
 			
 			isKeyword(input);
 			
@@ -182,7 +237,8 @@ void isWord(char * input){
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-void isComment(char * input){
+//Function for block comments; looks for the closing block
+void isCommentBlock(char * input){
 	while(currentLoc < strlen(input)){
 		if(currentLoc < strlen(input)-1 && input[currentLoc] == '*' && input[currentLoc+1] == '/'){
 			currentLoc += 2;
@@ -200,6 +256,69 @@ void isComment(char * input){
 		
 		currentLoc++;
 	}
+	
+	return;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Function for // comments; looks for a '\n' char
+void isCommentLine(char * input){
+	while(currentLoc < strlen(input)){
+		if(currentLoc < strlen(input)-1 && input[currentLoc+1] == '\n'){
+			currentLoc++;
+			startLoc = currentLoc;
+			
+			break;
+		}
+		
+		else if(currentLoc == strlen(input)-1){
+			currentLoc++;
+			startLoc = currentLoc;
+			
+			break;
+		}
+		
+		currentLoc++;
+	}
+	
+	return;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Function to detect an entire string. Will not do anything if there exists an invalid string.
+void isString(char * input){
+	while(currentLoc < strlen(input)){
+		//Detect if an excaped ''' or '"' exists before we check for an ending mark
+		if(currentLoc < strlen(input)-2 && input[currentLoc+1] == '\\' && input[currentLoc+2] == input[startLoc]){
+			currentLoc += 2;
+		}
+		//Detect if the string ends
+		else if(currentLoc < strlen(input)-1 && input[currentLoc+1] == input[startLoc]){
+			currentLoc++;
+			createNewNode(input, "string");
+			
+			break;
+		}
+		//Else check for an end to the entire input; if so, none of this is valid.
+		else if(currentLoc == strlen(input)-1){
+			printf("String invalid.\n");
+			
+			currentLoc = strlen(input);
+			startLoc = currentLoc;
+			
+			break;
+		}
+		
+		currentLoc++;
+	}
+	
+	return;
 }
 
 
@@ -208,15 +327,23 @@ void isComment(char * input){
 
 //Function to test for operators. 
 void isPunctuation(char * input){
-	//Check for comment before anything else
+	//Unfortunately, we have to check each and every single one of these one by one
+	//We can't break it up by size, because we don't know the size coming into this, and we can't blindly check the size first
+	
+	//Check for block comment before anything else
 	if(currentLoc < strlen(input)-1 && input[currentLoc] == '/' && input[currentLoc+1] == '*'){
 		currentLoc += 2;
-		isComment(input);
+		isCommentBlock(input);
 	}
-	
+	//Check for line comment before anything else
 	else if(currentLoc < strlen(input)-1 && input[currentLoc] == '/' && input[currentLoc+1] == '/'){
-		currentLoc = strlen(input);
-		startLoc = currentLoc;
+		currentLoc += 2;
+		isCommentLine(input);
+	}
+	//Check for quotes; there must be space an ending quote.
+	else if(currentLoc < strlen(input)-2 && (input[currentLoc] == '\'' || input[currentLoc] == '\"')){
+		currentLoc++;
+		isString(input);
 	}
 	
 	//All operators of size 3
@@ -517,7 +644,7 @@ void isDecimal(char * input){
 
 
 void isNumber(char * input){
-	//printf("Number.\n");
+	//printf("Number at %d\n", currentLoc);
 	
 	while(currentLoc < strlen(input)){
 		//Check to see if the string is a float with a decimal point.
@@ -603,8 +730,8 @@ void isHex(char * input){
 //Function to determine what type of token is required and sends it to the proper function
 void tokenType(char * input){
 	if(isspace(input[currentLoc])){
-		startLoc++;
 		currentLoc++;
+		startLoc = currentLoc;
 	} 
 	else if(isalpha(input[currentLoc])){
 		isWord(input);
@@ -633,6 +760,8 @@ void tokenType(char * input){
 
 //Function to print the entire linked list.
 void printList(){
+	//printf("printing\n");
+	
 	if(head == NULL){ printf("Head is null.\n"); }
 	Node * l = head;
 	while(l != NULL){
@@ -672,15 +801,19 @@ void freeList(){
 
 //Main function
 int main(int argc, char * argv[]){
-	//Node * head = NULL;
-	
 	//Check for exactly two arguments; one is not enough and more is too many
 	if(argc == 2){
+		//printf("strlen: %ld\n", strlen(argv[1]));
+		
+		char * test = "int main(int argc, char * argv[]){ printf(\"Hello there.\\n\"); }";
+		
 		//Tokenize the string input
+		//while(startLoc < strlen(test)){
 		while(startLoc < strlen(argv[1])){
-			//printf("i: %d, currentLoc: %d\n", i, currentLoc);
+			//printf("startLoc: %d, currentLoc: %d\n", startLoc, currentLoc);
 			
 			tokenType(argv[1]);
+			//tokenType(test);
 		}
 		
 		printList();
